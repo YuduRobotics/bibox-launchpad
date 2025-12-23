9d3f09772c99b59b7345320921cf3e2f42bd800d
gouthamsk98 <gouthamben10@gmail.com>
2025-12-23 17:34:56 +0530

    feat: Add CH572 to allowed USB port chipsets

--- js/utils.js
+++ js/utils.js
@@ -21,7 +21,7 @@ export const usb_Port_Filters = [
 ];
 
 //usb port open allow chipsets are
-export const usbPortOpenChipSets = ["CH592", "CH32X035"];
+export const usbPortOpenChipSets = ["CH592", "CH32X035", "CH572"];
 
 export function getTerminalColumns(mainContainer = null) {
   const mainContainerWidth = mainContainer?.offsetWidth || 1320;
